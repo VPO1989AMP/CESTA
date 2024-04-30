bded6f91b3ac0163dc6eb3eb91464a7c78202715
Victor_PB <victorpb@me.com>
2024-04-30 17:08:23 +0200

    pagar

--- front/src/components/Cesta.jsx
+++ front/src/components/Cesta.jsx
@@ -1,12 +1,42 @@
-import {useContext} from 'react'
+import {useContext, useState, useEffect} from 'react'
 import {Context} from './App'
+import { ethers } from 'ethers';
+import { Link} from 'react-router-dom'
 
 export function Cesta() {
     const [estado, setEstado] = useContext(Context)
-    const total = estado.cesta.reduce((acc, item) => {
-        return acc + item.total;
-    }, 0);
-    
+    const [cuenta, setCuenta] = useState(null)
+    const [txOk, setTxOk] = useState(null)
+    const [txKo, setTxKo] = useState(null)
+    const total = estado.cesta.reduce((acc, item) => acc + item.total, 0)
+    useEffect(()=>{
+        window.ethereum && window.ethereum.request({
+            method: 'eth_requestAccounts'
+        }).then (cuentas =>{
+            setCuenta(cuentas[0])
+            window.ethereum.on("accountsChanged", (cuentas) => {
+                setCuenta(cuentas[0])
+            })
+        })
+    })
+async function pagar(){
+    const txParams = {
+        to: "0xED21900caD24632a447d6889aac4D868aBAA68bA",
+        from: cuenta,
+        value: ethers.utils.parseEther(total.toString()).toHexString(),
+    }
+    console.log(txParams)
+    try{
+        const tx=await ethereum.request({
+            method:"eth_sendTransaction",
+            params:[txParams]
+        })
+        setTxOk(tx)
+    }catch(error){
+        setTxKo(error)
+    }
+}
+
     return(
         <div>
             <table className="table">
@@ -22,7 +52,11 @@ export function Cesta() {
                 <tbody>
                     {estado.cesta.map(i=>(
                         <tr key={i.producto.ProductID}>
-                            <td>{i.producto.ProductID}</td>
+                            <td>
+                                <Link to={`/productos/${i.producto.ProductID}`}>
+                                    {i.producto.ProductID}
+                                </Link>
+                            </td>
                             <td>{i.producto.ProductName}</td>
                             <td>{i.producto.UnitPrice}</td>
                             <td>{i.cantidad}</td>
@@ -31,7 +65,11 @@ export function Cesta() {
                     ))}
                 </tbody>
             </table>
-            <h3>TOTAL {total}</h3>      
+            <h3>TOTAL {total}</h3>
+            <h3>{cuenta}</h3>      
+            <button onClick={()=> pagar()} className='btn btn-primary'>Pagar</button>
+            {txOk && <p className="alert alert-success">{txOk}</p>}
+            {txKo && <p>{txKo}</p>}
         </div>
     )
 }
